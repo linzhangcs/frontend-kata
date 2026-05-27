1bacb302ace026ca2342335ffc44dd9a0f04231d
Lin Zhang <lin.zhang.ux@gmail.com>
2026-05-26 20:43:27 -0700

    add filter test for mutation and thiArg

--- problems/js/filter/solution.test.js
+++ problems/js/filter/solution.test.js
@@ -6,18 +6,33 @@ beforeAll(() => {
 })
 
 describe("filter", () => {
-  it('Should return empty array when no matches', () => {
+  it("Should return empty array when no matches", () => {
     expect([1, 3, 5, 7].myFilter(n => n % 2 === 0)).toEqual([]);
   })
-  it('Should filter items based on callback result', () => {
+
+  it("Should filter items based on callback result", () => {
     expect(['a', 'ab', 'c', 'abc'].myFilter(item => item.length > 1)).toEqual(['ab', 'abc']);
   })
 
-  // it('Should pass item, index, and array to callback', () => {
-  //   expect([1, 2, 3, 4].myFilter((_, index) => index > 1)).toEqual([3, 4]);
-  // })
-  // it('Should pass thisArg to callback', () => {
-  //
-  // }))
+  it("Should pass item, index, and array to callback", () => {
+  })
+
+  it("Should not mutate the original array", ()=>{
+    const numbers = [1, 2, 3];
+    const greaterThanTwo = numbers.myFilter(number => number > 1);
+    expect(numbers).toEqual([1, 2, 3]);
+    expect(greaterThanTwo).toEqual([2, 3]);
+  })
+
+  it("Should pass thisArg to callback", () => {
+    const context = {min: 3};
+
+    const input = [1, 2, 3, 4, 5];
+    function isGreaterThanMin(number){
+      return number > this.min;
+    }
+
+    expect(input.myFilter(isGreaterThanMin, context)).toEqual([4, 5]);
+  })
 
 })

50c8e3f0349e8554eae16585b6c5a367c55a0215
Lin Zhang <lin.zhang.ux@gmail.com>
2026-05-29 23:01:35 -0700

    chore: move searchData out of the component

--- src/problems/components/debounced-search/DebouncedSearch.jsx
+++ src/problems/components/debounced-search/DebouncedSearch.jsx
@@ -2,6 +2,14 @@ import { useState, useMemo} from "react";
 import debounce from "../../async/rate-limiting/debounce/solution.js"
 import {fruits} from './data.js'
 
+// Helper function that doesn't depend on changing component state, move out of the component so it's not recreated on every render.
+function searchData(searchTerm) {
+  const normalizedSearchTerm = searchTerm.trim().toLowerCase();
+
+  if(normalizedSearchTerm.length === 0) return [];
+  return fruits.filter(fruit => fruit.toLowerCase().includes(normalizedSearchTerm));
+}
+
 const DebouncedSearch = () => {
   const [search, setSearch] = useState("");
   const [results, setResults] = useState([]);
@@ -9,15 +17,16 @@ const DebouncedSearch = () => {
   const debouncedUpdateResult = useMemo(() =>
       debounce((searchTerm)=> setResults(searchData(searchTerm)), 300), []);
 
-  function searchData(searchTerm) {
-    const normalizedSearchTerm = searchTerm.trim().toLowerCase();
-
-    if(normalizedSearchTerm.length === 0) return [];
-    return fruits.filter(fruit => fruit.toLowerCase().includes(normalizedSearchTerm));
-  }
-
   function handleSearchInput(e){
-    setSearch(e.target.value);
+    const nextSearchTerm = e.target.value;
+    setSearch(nextSearchTerm);
+
+    if(nextSearchTerm.trim().length === 0){
+      setResults([]);
+      //TODO: cancel debounced is needed
+      return;
+    }
+
     debouncedUpdateResult(e.target.value);
   }
 

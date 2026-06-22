17037effc63ac9c8f66c9fef7b92b369edd3c800
Lin Zhang <lin.zhang.ux@gmail.com>
2026-06-21 22:55:01 -0700

    add initial solution for groupby

---
+++ src/problems/js/groupby/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Array<unknown>} array The array to iterate over.
+ * @param {(value: unknown) => string | number} iteratee The iteratee to transform keys.
+ * @returns {Record<string, Array<unknown>>} Returns the composed aggregate object.
+ */
+export default function groupBy(array, iteratee) {
+  const result = {};
+
+  array.forEach(item => {
+    const key = iteratee(item);
+    if(result[key] === undefined) {
+      result[key] = [];
+    }
+    result[key].push(item);
+  })
+
+  return result;
+}
